e68b27bdc2d195d0be379facca34dc5da1b59191
Lee Santer <info@leesanter.co.uk>
2025-08-25 22:46:06 +0100

    feat/integrations-mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
 	site: process.env.PUBLIC_SITE_URL || 'https://example.com',
-	integrations: [sitemap()],
+	integrations: [sitemap(), mdx()],
 	image: {
 		service: {
 			// If `sharp` is installed, this just works.
